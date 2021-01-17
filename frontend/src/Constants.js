e91b4a4f1ceeaa4fed4e80806c7826dbd1de605b
eal001 <eal001@ucsd.edu>
2021-01-16 17:46:57 -0800

    ed to revert map file to class implementation

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL = "http://localhost:8000"
 export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
-
+export const GOOGLE_MAP_URL = "https://maps.googleapis.com/maps/api/js?key=APIKEY&v=3.exp&libraries=geometry,drawing,places"
 //console.log(GOOGLE_API_KEY)
--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,11 +1,12 @@
 import React, { Component, useState} from 'react';
 import AddressInfo from './addressinfo';
 import LeaseInfo from './LeaseInfo';
-import {Map, Marker, GoogleApiWrapper, InfoWindow} from 'google-maps-react'
+import {Map, Marker, GoogleApiWrapper, InfoWindow, GoogleMap, withScriptjs, withGoogleMap} from 'google-maps-react'
 import Item from 'antd/lib/list/Item';
 import {Button} from 'antd'
 import { render } from 'react-dom';
-import { GOOGLE_API_KEY } from '../Constants'
+import { GOOGLE_API_KEY , GOOGLE_MAP_URL} from '../Constants'
+import { convertLegacyProps } from 'antd/lib/button/button';
 
 
 const mapStyles = {
@@ -13,85 +14,62 @@ const mapStyles = {
   height: '90%'
 }
 
+const initialCenter = {
+  lat: 38.5382,
+  lng: -121.7617
+}
 // const containerStyle = {
 //   //position:,  
 //   width: '100%',
 //   height: '90%'
 // }
 
-const PseudoMap = (props) =>  {
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     showingAddressBar: false,
-  //     showingInfoWindow: false,
-  //     selectedPlace: {}
-  //   };
-  //   this.onMouseoverMarker = this.onMouseoverMarker.bind(this);
-  //   this.onMouseclick = this.onMouseclick.bind(this);
-  // }
+export class PseudoMapContainer extends Component {
 
-  const [currentLocation, setLocation] = useState(null);
-  const [drawerVisible, setVisible] = useState(false);
-
-  const onMouseoverMarker = ({location}) =>{
-    this.setState({
-      selectedPlace: location,
-      showingInfoWindow: true
-    })
+  constructor(props){
+    super(props);
+    this.state = {
+      currentLocation: null, 
+      sideBarVisible: false, 
+      messageVisible: false
+    }
   }
 
-  const onMouseclick = ({location}) =>{
-    this.setState({
-      selectedPlace: location,
-      showingInfoWindow: false,
-      showingAddressBar: true
-    })
+  handleMouseOver(){
+
   }
-  
-  console.log(props.google);
-  
-  return (
-      <div>
-        <h1>
-          bruh
-        </h1>
-        <>{console.log(props)}</>
-        <Map
-          google={props.google}
-          zoom={12}
-          style={mapStyles}
-          //containerStyle = {containerStyle}
-          initialCenter={
-            {
-              lat: 38.5382,
-              lng: -121.7617
-            }
-          }
-        >
-        {
-          this.props.locations.map(marker => {
-            console.log(marker);
-            return(
-              <Marker key={marker.name} position={{lat : marker.lat, lng: marker.long}} onMouseover={this.onMouseoverMarker(marker)} onClick = {this.onMouseclick(marker)}/>
-            )
-          })
-        }
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible = {this.state.showingInfoWindow}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-          </InfoWindow>
-        </Map>  
-      </div>
+
+  handleClick(){
+
+  }
+  render() {
+    return (
+      <Map
+        google = {this.props.google}
+        zoom = {12}
+        initialCenter = {initialCenter}
+        mapStyles = {mapStyles}
+      >
+        {this.props.locations.map(loc => {
+          console.log("rnu")
+          return(
+            <>
+              <Marker key = {loc.name} position = {{lat: loc.lat, lng: loc.long}}  onMouseover = {this.handleMouseOver} onClick = {this.handleClick} />
+            </>
+          )
+        })}
+      </Map>
+
+
     )
   }
+    
+  
+}
 
 
 //once google api is implemented, we will be able to call this a real map
 export default GoogleApiWrapper({
   apiKey: GOOGLE_API_KEY
-})(PseudoMap); //change the name of this later
+})(PseudoMapContainer); //change the name of this later
