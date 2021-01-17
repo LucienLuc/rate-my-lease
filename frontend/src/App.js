b3c655d28b9e4ca02cb6c5c6d364db8df6bd66bb
jiuchoe4 <jiuchoe@ucdavis.edu>
2021-01-16 23:04:30 -0800

    fixed polling issue on first render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,8 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      query : {}
+      query : {},
+      isLoaded: false
     }
   }
 
@@ -24,17 +25,19 @@ class App extends React.Component {
     axios
       .get(BASE_URL + '/api/location-all')
       .then(response => {
-          this.setState({query: response.data})
-          console.log(this.state.query);
+          this.setState({query: response.data, isLoaded: true})
       })
   }
-  
+
   changeQuery = (newQuery) => {
     this.setState({query : newQuery.data})
     console.log(this.state.query);
   }
 
   render() {
+    if(!this.state.isLoaded){
+      return null
+    }
     return (
       <LoadScript
       googleMapsApiKey= {GOOGLE_API_KEY}
